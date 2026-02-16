b2575ff01de8d479d164b78f067298a02f054046
MarcoR5 <marcowork50@gmail.com>
2026-02-16 16:38:06 +0200

    t17

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: 'export',
-  // distDir: 'dist',
+    images:{
+        unoptimized:true
+    },
     eslint: {
     ignoreDuringBuilds: true,
   },
